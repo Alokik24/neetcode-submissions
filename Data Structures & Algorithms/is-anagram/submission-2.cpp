a5dee70ec74e5b7c946a8ba7bd11b7ea43c6da56
Alokik Garg <137095971+Alokik24@users.noreply.github.com>
2026-06-26 14:10:15 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.size() != t.size())return false;
+
+map<char,int>freq_s;
+map<char,int>freq_t;
+
+for(int i=0; i <s.size(); i++){
+    freq_s[s[i]]++;
+    freq_t[t[i]]++;
+}
+return freq_s == freq_t;
+}
+};
